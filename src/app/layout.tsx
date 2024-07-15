d0f647f924f6e7a5152af96ae25f2b68160a7a96
João-Eduardo-AJ <joaoeduaj@gmail.com>
2024-07-15 12:43:10 -0300

    feat(root): creates development environment

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter, Rethink_Sans } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  display: "swap",
+  style: "normal",
+  subsets: ["latin"],
+  variable: "--font-inter"
+});
+
+const rethink_sans = Rethink_Sans({
+  display: "swap",
+  style: "normal",
+  subsets: ["latin"],
+  variable: "--rethink-sans"
+});
+
+export const metadata: Metadata = {
+  title: "Home Connect",
+  description: ""
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${rethink_sans.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#13161A",
+        secondary: "#606A73",
+        disabled: "#818D99",
+
+        danger: "#EA1246",
+        dangerLight: "#FFEEF2",
+
+        success: "#0ACE88",
+        successLight: "#EEFFF9",
+
+        highlight: "#695ACC",
+        HighlightAux: "#2CB8E0",
+
+        borderPrimary: "#DDE4EB",
+        grayScale50: "#F5F7FA"
+      }
+    }
+  },
+  plugins: []
+};
+export default config;
