4295dd2a11d7f6e1f7ae4a60290a63bfab38f6c9
João-Eduardo-AJ <joaoeduaj@gmail.com>
2024-08-14 22:48:24 -0300

    chore(src/shared): update components folder local

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./button";
-export * from "./input";
-export * from "./logo";
-export * from "./search-input";
-export * from "./typography";
--- src/components/logo/index.tsx
+++
@@ -1,5 +0,0 @@
-export const Logo = () => (
-  <div className="logo group relative h-8 w-8 rounded-full duration-1000 hover:rotate-90">
-    <div className="absolute right-0 top-[6px] h-5 w-5 -rotate-45 rounded bg-gradient-to-bl from-highlight to-highlight-aux transition-all duration-1000 group-hover:right-[9px] group-hover:top-2 group-hover:h-4 group-hover:w-3 group-hover:rotate-0 group-hover:rounded-none"></div>
-  </div>
-);
