46c7b316dd0fa276acb9ac7dc02136585d00ef1b
João-Eduardo-AJ <joaoeduaj@gmail.com>
2024-08-22 22:05:10 -0300

    feat(src/app): adds empty state to client list

--- src/app/(listing)/clients/page.tsx
+++ src/app/(listing)/clients/page.tsx
@@ -1,6 +1,43 @@
-import { MainListing } from "../_components";
+import { Bell, PlusCircle } from "@phosphor-icons/react/dist/ssr";
+import { IconButton, MainListing } from "../_components";
+import { Button, SearchInput, Typography } from "@/app/_components";
+import Image from "next/image";
+import noDataFound from "@/../public/no-data-found.svg";
 
 function Clients() {
-  return <MainListing>clients</MainListing>;
+  return (
+    <MainListing>
+      <IconButton className="group self-end bg-gray-scale-50">
+        <Bell
+          size={20}
+          className="duration-500 group-hover:scale-110 group-hover:animate-bell-icon"
+        />
+      </IconButton>
+      <div className="flex items-center justify-between">
+        <Typography variant="h2">Clients</Typography>
+        <div className="flex gap-4">
+          <SearchInput placeholder="Search by name" />
+          <Button>
+            <PlusCircle size={20} className="-mr-2" weight="bold" />
+            Add new client
+          </Button>
+        </div>
+      </div>
+      <div className="flex h-full w-full flex-col items-center justify-center gap-4 rounded-lg bg-white">
+        <Image
+          src={noDataFound}
+          width={336}
+          height={336}
+          alt="no clients found"
+          priority
+        />
+        <Typography>No clients found</Typography>
+        <Button variant={{ type: "text", color: "highlight" }}>
+          <PlusCircle size={20} className="-mr-2" weight="bold" />
+          Add Requirement
+        </Button>
+      </div>
+    </MainListing>
+  );
 }
 export default Clients;
